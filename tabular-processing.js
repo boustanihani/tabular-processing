576b5a3204e676fd63a8f1bdda2fdd15e8e83b0d
Hani <boustanihani@gmail.com>
2015-01-20 01:38:27 +0100

    first commit

---
+++ tabular-processing.js
@@ -0,0 +1,46 @@
+Books = new Mongo.Collection('books');
+
+TabularTables = {};
+
+TabularTables.Books = new Tabular.Table({
+    name: "BookList",
+    collection: Books,
+    columns: [{
+        data: "title",
+        title: "Title"
+    }, {
+        data: "author",
+        title: "Author"
+    }, ]
+});
+
+if (Meteor.isClient) {
+
+    Template.registerHelper('TabularTables', TabularTables);
+
+    Template.registerHelper('processing', function() {
+        return Session.get('processing');
+    });
+
+    // https://datatables.net/reference/event/processing
+    $(window).on('processing.dt', function(e, settings, proc) {
+        console.log('processing.dt=' + proc);
+        Session.set('processing', proc);
+    });
+
+}
+
+if (Meteor.isServer) {
+    var count = Books.find().count();
+    console.log('Books count:', count);
+
+    if (count == 0) {
+
+        for (var i = 1; i <= 200; i++) {
+            Books.insert({
+                title: 'Titel-' + i,
+                author: 'Author-' + i
+            });
+        }
+    }
+}
